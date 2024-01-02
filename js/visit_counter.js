59b4a988b2d54f70a570b78921dc4e777118708a
Mohammad Ghavidel <58626257+Slmaking@users.noreply.github.com>
2024-01-02 12:18:32 -0500

    Update visit_counter.js

--- js/visit_counter.js
+++ js/visit_counter.js
@@ -2,10 +2,14 @@ function generateUniqueID() {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
 
+// Offsets
+const UNIQUE_VISITOR_OFFSET = 108;
+const VISIT_COUNT_OFFSET = 510;
+
 // Retrieve or initialize the unique visitor count
 let uniqueVisitors = localStorage.getItem('unique_visitors');
 if (uniqueVisitors === null) {
-    uniqueVisitors = 108; // Start from 108 if not previously set
+    uniqueVisitors = 0; // Start from 0 if not previously set
     localStorage.setItem('unique_visitors', uniqueVisitors);
 } else {
     uniqueVisitors = parseInt(uniqueVisitors);
@@ -25,13 +29,13 @@ if (visitorID === null) {
 // Retrieve or initialize the visit count
 let visitCount = localStorage.getItem('visit_counter');
 if (visitCount === null) {
-    visitCount = 500; // Start from 500 if not previously set
+    visitCount = 0; // Start from 0 if not previously set
 } else {
     visitCount = parseInt(visitCount);
 }
 visitCount++; // Increment visit count
 localStorage.setItem('visit_counter', visitCount);
 
-// Display the counts
-document.getElementById('uniqueVisitors').innerHTML = `${uniqueVisitors} visitors and`;
-document.getElementById('visitCounter').innerHTML = `${visitCount} views since 2022`;
+// Display the counts with offsets
+document.getElementById('uniqueVisitors').innerHTML = `${uniqueVisitors + UNIQUE_VISITOR_OFFSET} visitors and`;
+document.getElementById('visitCounter').innerHTML = `${visitCount + VISIT_COUNT_OFFSET} views since 2022`;
